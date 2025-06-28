31798bfebb650f35bdc2d1f7419a6acd42588b38
TheCoderAayan <mujawaraayan9@gmail.com>
2025-06-28 23:49:25 +0530

    Add files via upload
    first project in java

---
+++ myfirst.java
@@ -0,0 +1,14 @@
+package first;
+
+public class myfirst {
+
+	public static void main(String[]args)
+	{
+		
+		System.out.println("    /| ");
+		System.out.println("   / |");
+		System.out.println("  /  | ");
+		System.out.println(" /___|");
+		
+	}
+}
