e89273e212d97f525891fe053d052100258c6cfa
TheCoderAayan <mujawaraayan9@gmail.com>
2025-07-05 23:53:34 +0530

    Arrays using Java

---
+++ Array.java
@@ -0,0 +1,24 @@
+package variables;
+
+
+
+public class Array {
+
+	public static void main(String[]args) {
+		
+//		String[] friends= {"Mike","Karen","Kevin"};
+//		System.out.println(friends[0]);
+//		System.out.println(friends.length);	
+		
+		String[]friends=new String[3];
+		friends[0]="MIke";
+		friends[1]="Keran";
+		friends[2]="Kevin";
+		
+		
+		System.out.println(friends[0]);
+	System.out.println(friends.length);	
+		
+		
+		}	
+}
