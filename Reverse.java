f4a07ace86bbee2a45052511b503c51be501d947
TheCoderAayan <mujawaraayan9@gmail.com>
2025-07-07 11:41:22 +0530

    Reverse the string

---
+++ Reverse.java
@@ -0,0 +1,28 @@
+package com.sunbeam;
+import java.util.Scanner;
+public class Reverse {
+
+	public static void main(String[] args) {
+		
+
+
+		
+		        Scanner sc = new Scanner(System.in);
+		        System.out.print("Enter a string: ");
+		        String input = sc.nextLine();
+
+		        String reversed = "";
+
+		        for (int i = input.length() - 1; i >= 0; i--) {
+		            reversed = reversed + input.charAt(i);
+		        }
+
+		        System.out.println("Reversed string: " + reversed);
+	sc.close();	    
+	}
+		
+
+
+	}
+
+
